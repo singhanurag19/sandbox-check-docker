d79bd512507b24a3058e1ca5bfa983258edb2786
Anurag Singh <41174232+singhanurag19@users.noreply.github.com>
2023-10-26 16:34:58 +0530

    Complete the initial setup

---
+++ src/main/java/com/example/tesapi/TesapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesapiApplication.class, args);
+	}
+
+}
